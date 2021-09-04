5ddbb20d03abae1bc5804b390b9adeab4f445053
xynee <90083091+xynee@users.noreply.github.com>
2021-09-04 01:50:32 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/************************************************************************* Java Program Name
+************************************************************************** Student Name (first and last name)
+* Date created
+* Course Number and Section Number
+************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");
+		System.out.println();
+	}
+}
